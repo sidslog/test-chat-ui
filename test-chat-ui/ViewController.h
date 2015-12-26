c826a2a9a75d9c661a0a5ad68b9b5393cca358c6
Sergey Sedov <sidslog@gmail.com>
2015-12-26 13:49:28 +0300

    Initial Commit

---
+++ test-chat-ui/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test-chat-ui
+//
+//  Created by Sergey Sedov on 12/26/15.
+//  Copyright © 2015 Sergey Sedov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test-chat-ui/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test-chat-ui
+//
+//  Created by Sergey Sedov on 12/26/15.
+//  Copyright © 2015 Sergey Sedov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
